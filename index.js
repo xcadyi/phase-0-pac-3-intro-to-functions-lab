adc88900038464ad025acead27b2985cf99f3685
Selin Ayyildiz <xcadyix@gmail.com>
2022-04-11 15:09:04 -0400

    commit tree d1fb9af63f60cbaba19858501eb19e3cdc158de5

---
+++ index.js
@@ -0,0 +1,14 @@
+
+function shout( string ) {
+console.log (shout)
+ return string.toUpperCase();
+    console.log( shout );  
+}
+function whisper( string ) {
+console.log (whisper)
+return string.toLowerCase();
+console.log( whisper );
+}
+const string = Hello
+
+
